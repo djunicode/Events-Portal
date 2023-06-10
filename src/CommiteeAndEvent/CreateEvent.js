78d2f5fc453eabf6c1c320579053d01fb467790e
Shubham <shubhammehta2003@gmail.com>
2023-06-10 22:51:06 +0530

    create event page added

---
+++ src/CommiteeAndEvent/CreateEvent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import WithSubnavigation from '../Navbar/NavMain';
+// import WithSubnavigation from '../Navbar/NavMain';
+
+
+const CreateEvent = () => {
+    return (
+        <>
+            <WithSubnavigation />
+            
+
+        </>
+    )
+}
+
+export default CreateEvent;
